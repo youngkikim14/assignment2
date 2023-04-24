7105389b1783074adc2d8a63599f5fafbfa630f1
youngkikim <rkfcl1169@gmail.com>
2023-04-25 00:55:04 +0900

    UserService 구현 및 jwt 토큰 부분 구현

--- src/main/java/com/sparta/assignment/entity/User.java
+++ src/main/java/com/sparta/assignment/entity/User.java
@@ -1,4 +1,26 @@
 package com.sparta.assignment.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
--- src/main/java/com/sparta/assignment/service/UserService.java
+++ src/main/java/com/sparta/assignment/service/UserService.java
@@ -1,10 +1,52 @@
 package com.sparta.assignment.service;
 
+import com.sparta.assignment.dto.SigninRequestDto;
+import com.sparta.assignment.dto.SignupRequestDto;
+import com.sparta.assignment.entity.User;
+import com.sparta.assignment.jwt.JwtUtil;
+import com.sparta.assignment.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private final
+
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+
+    @Transactional
+    public void signup(SignupRequestDto signupRequestDto){
+        String username = signupRequestDto.getUsername();
+        String password = signupRequestDto.getPassword();
+
+        Optional<User> found = userRepository.findByUsername(username);
+        if (found.isPresent()){
+            throw new IllegalArgumentException("중복 된 아이디입니다"); //그런데 왜 굳이 IllegalArgumentException 써야 할까? 어차피 조건에서 중복 아이디 처리됐는데..
+            }
+        User user = new User(username, password); // 생성자 주입하면 혹시 생략 가능한가?
+        userRepository.save(user);
+    }
+
+    @Transactional
+    public void signin(SigninRequestDto signinRequestDto, HttpServletResponse response) {
+        String username = signinRequestDto.getUsername();
+        String password = signinRequestDto.getPassword();
+
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
+                    );
+
+        if(!user.getPassword().equals(password)){
+            throw  new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(user.getUsername()));
+    }
+
+
+
 }
