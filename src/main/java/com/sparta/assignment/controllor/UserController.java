6121f0b6b97414ae8d7937e1366cd605089ec97d
youngkikim <rkfcl1169@gmail.com>
2023-04-25 21:09:55 +0900

    scret key 추가

--- src/main/java/com/sparta/assignment/controllor/UserController.java
+++ src/main/java/com/sparta/assignment/controllor/UserController.java
@@ -6,11 +6,12 @@ import com.sparta.assignment.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
 public class UserController {
@@ -18,13 +19,13 @@ public class UserController {
     private final UserService userService;   // userservice랑 연결, 생성자 주입을 통한 DI
 
     @PostMapping("/signup")
-    public String signup(@Valid SignupRequestDto signupRequestDto) {
+    public String signup(@Valid @RequestBody SignupRequestDto signupRequestDto) {
         userService.signup(signupRequestDto);
         return "회원가입 완료!";
     }
 
     @PostMapping("/signin") // signin에 필요한 dto값과 jwt 토큰을 함께 서비스로 전달
-    public String signin(SigninRequestDto signinRequestDto, HttpServletResponse response){
+    public String signin(@RequestBody SigninRequestDto signinRequestDto, HttpServletResponse response){
         userService.signin(signinRequestDto, response);
         return "로그인 완료!";
     }
